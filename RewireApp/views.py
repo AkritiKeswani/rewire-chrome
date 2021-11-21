3a494d44ff3667dd7f65e9b242039a00b7e8cf3f
Akriti Keswani <akritikeswani76@gmail.com>
2021-11-21 02:17:10 -0500

    adding some static stuff

--- RewireApp/views.py
+++ RewireApp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,3 +6,5 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
     return render(request, 'app/base.html')
+
+
