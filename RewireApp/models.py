408293892032b947b703f108e9a06961f4126406
Akriti Keswani <akritikeswani76@gmail.com>
2021-11-21 01:23:49 -0500

    models.py progress bro

--- RewireApp/models.py
+++ RewireApp/models.py
@@ -1,3 +1,17 @@
+"""
+models.py
+""" 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Flow(models.Model):
+    startingUser = models.CharField(max_length=50)
+    targettingUser = models.CharField(max_length=50)
+    focusStartTime = models.DateTimeField(auto_now_add=True)
+    focusEndTime = models.DateTimeField(auto_now_add=True)
+    #figure out what else to add 
+
+
+
+
