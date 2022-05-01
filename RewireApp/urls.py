b7a8ad7799250d9796b05f139d58ea3371abdeb0
vivekkhimani <vivekkhimani07@gmail.com>
2022-04-30 22:24:04 -0700

    auth flow set up

--- RewireApp/urls.py
+++ RewireApp/urls.py
@@ -4,26 +4,21 @@ from django.urls import include, path  # new
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from RewireApp.views import CustomObtainTokenPairView
+from RewireApp.views import CustomObtainTokenPairView, RegisterView
 
 # api routing
 router = DefaultRouter()
 
 
 # app routing
-authpatterns = []  # routes for authentication
-
-jwtpatterns = [
-    path("token/", CustomObtainTokenPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+authpatterns = [
+    path("login/", CustomObtainTokenPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", RegisterView.as_view(), name="auth_register"),
 ]
 
-urlpatterns = (
-    [
-        path("", include(router.urls)),
-        path("admin/", admin.site.urls),
-        path("accounts/", include("django.contrib.auth.urls")),  # new
-    ]
-    + authpatterns
-    + jwtpatterns
-)  # routes for the app
+urlpatterns = [
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+] + authpatterns  # routes for the app
--- RewireApp/views.py
+++ RewireApp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 import logging
 
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 from RewireApp.serializers import CustomTokenObtainPairSerializer
+from RewireApp.serializers import RegisterSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -12,3 +14,9 @@ logger = logging.getLogger(__name__)
 class CustomObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
